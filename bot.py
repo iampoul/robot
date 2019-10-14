30c8d5445f2ba9d80abdcc4ba505063f85d6f722
ipoul <poul@encore.dk>
2019-10-14 08:56:27 +0200

    uptime and rearranged code

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
-import logging
 import os
 import time
-import wolframalpha
-
-from duckduckpy import query
 from slackclient import SlackClient
+from duckduckpy import query
+import wolframalpha
+import logging
 
 
 class Robot:
@@ -15,9 +14,38 @@ class Robot:
         self.bot_id = self.get_bot_id()
         self.bot_start_time = time.time()
         self.read_delay = 0.2
-        self.version = '0.1.0 (alpha)'
+        self.version = '0.5.1 (alpha)'
         self.COMMANDS_MAP = self.build_commands_map()
         self.wa_client = wolframalpha.Client(os.environ.get('WA_TOKEN'))
+
+    def build_commands_map(self):
+        return {
+            '!help': {
+                'function': 'command_help',
+                'example': '!help',
+                'description': 'Displays a list of valid commands'
+            },
+            '!version': {
+                'function': 'command_version',
+                'example': '!version',
+                'description': 'Show <@{}>\'s version'.format(self.name)
+            },
+            '!wiki': {
+                'function': 'command_wiki',
+                'example': '!wiki <search phrase>',
+                'description': 'returns a single search result from wiki (via DuckDuckGo)'
+            },
+            '<@{}>'.format(self.bot_id): {
+                'function': 'command_robot',
+                'example': '@{} <question>'.format(self.name),
+                'description': 'chat with {}'.format(self.name)
+            },
+            '!uptime': {
+                'function': 'command_uptime',
+                'example': '!uptime',
+                'description': 'Show <@{}>\'s uptime'.format(self.name)
+            },
+        }
 
     def get_bot_id(self):
         if __name__ == "__main__":
@@ -53,29 +81,20 @@ class Robot:
                     return output['text'], output['channel']
         return None, None
 
-    def build_commands_map(self):
-        return {
-            '!help': {
-                'function': 'command_help',
-                'example': '!help',
-                'description': 'Displays a list of valid commands'
-            },
-            '!version': {
-                'function': 'command_version',
-                'example': '!version',
-                'description': 'Show <@{}>\'s version'.format(self.name)
-            },
-            '!wiki': {
-                'function': 'command_wiki',
-                'example': '!wiki <search phrase>',
-                'description': 'returns a single search result from wiki (via DuckDuckGo)'
-            },
-            '<@{}>'.format(self.bot_id): {
-                'function': 'command_robot',
-                'example': '@{} <question>'.format(self.name),
-                'description': 'chat with {}'.format(self.name)
-            },
-        }
+    def command_help(self, command):
+        response = ''
+        if len(command.partition(' ')[2]) > 0:
+            argument = '!' + command.partition(' ')[2]
+            if argument in self.COMMANDS_MAP:
+                response += '`{}` - _{}_'.format(
+                    self.COMMANDS_MAP[argument]['example'],
+                    self.COMMANDS_MAP[argument]['description']) + '\n'
+        else:
+            for cmd in self.COMMANDS_MAP:
+                response += '`{}` - _{}_'.format(
+                    self.COMMANDS_MAP[cmd]['example'],
+                    self.COMMANDS_MAP[cmd]['description']) + '\n'
+        return response
 
     @staticmethod
     def command_wiki(command):
@@ -95,6 +114,9 @@ class Robot:
             else:
                 return "No results found"
         return "?"
+
+    def command_version(self, _):
+        return 'Botbot {}'.format(self.version)
 
     def command_robot(self, command):
         new_list = command.split()
@@ -121,8 +143,31 @@ class Robot:
         else:
             return 'Sir ?'
 
-    def command_version(self, _):
-        return 'Botbot {}'.format(self.version)
+    def command_uptime(self, _):
+        bot_command_time = time.time()
+        time_diff_from_start = bot_command_time - self.bot_start_time
+        m, s = divmod(time_diff_from_start, 60)
+        h, m = divmod(m, 60)
+        d, h = divmod(h, 24)
+        return self.days((int(d), int(h), int(m), int(s)))
+
+    @staticmethod
+    def days(t):
+        oxford_comma = False
+        a = [" day", " hour", " minute", "second"]
+        s = []
+        if len(t) != 4:
+            return "incorrect format"
+        else:
+            for i in range(4):
+                if t[i] != 0:
+                    plural = "s" if t[i] > 1 else ""
+                    s.append(str(t[i]) + a[i] + plural + ", ")
+            if len(s) > 1:
+                if not oxford_comma:
+                    s[-2] = s[-2].replace(",", "")
+                s.insert(len(s) - 1, "and ")
+            return "".join(s)[:-2]
 
     def start(self):
         if __name__ == "__main__":
